e5684c177be20195e77912a650adb792b383b787
Metric <aaronklick@gmail.com>
2019-07-27 23:53:50 -0500

    Quick fix for depth.glsl
    Quick fix for depth.glsl accidentally missing the 4 from vec(0).

--- AddToOutputDir/Shaders/Frag/depth.glsl
+++ AddToOutputDir/Shaders/Frag/depth.glsl
@@ -3,6 +3,6 @@ layout (location = 0) out vec4 FragColor;
 layout (location = 1) out vec4 Brightness;
 
 void main() {
-    Brightness = vec(0);
+    Brightness = vec4(0);
     FragColor = vec4(vec3(gl_FragCoord.w), 1);
 }
